dc5905cf4c8d9292436f79aa263b21dd5005d80c
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2025-02-19 12:11:20 +0530

    Fixed vercel config errors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,25 +11,38 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = process.env.FRONTEND_URLS?.split(",");
+const orginsAllowed = process.env.FRONTEND_URLS?.split(",");
 
-const corsOptions: cors.CorsOptions = {
-    origin: (origin, callback) => {
-        // console.log(origin);
-        // console.log(allowedOrigins);
-        if (!origin || (allowedOrigins && allowedOrigins.includes(origin))) {
-            // console.log("control reached here")
-            callback(null, true);
+const allowedCors:cors.CorsOptions ={
+    origin: (origin, callback)=>{
+
+        //1. Allow for same origin, server req's
+        if(!origin) {
+            return callback(null,true);
+        } 
+
+        //2. IN dev mode, allow all origin, or check allowedORgins array
+        if(process.env.NODE_ENV=="development"|| orginsAllowed?.indexOf(origin)!==-1) {
+            callback(null,true); 
         } else {
-            callback(new Error("Not allowed by CORS"));
+            callback(new Error("Not allowed by cors"))
         }
+
     },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization","token"], 
-};
+    //3. Allow, cookies, methods, headers
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"],
+    allowedHeaders:["Content-type", "Authorization", "token"]
 
 
-app.use(cors(corsOptions));
+
+
+        
+
+
+}
+
+app.use(cors());
 
 app.get("/", (req,res)=> {
     res.send("Lessgoo working!")
